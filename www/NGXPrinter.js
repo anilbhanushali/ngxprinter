afbd4e0b1567399acecd2cbff826a746969bda12
Anil Bhanushali <b.anil@better-life.co.in>
2015-02-08 21:00:57 +0530

    fixed show device list issue  Please enter the commit message for your changes. Lines starting

--- www/NGXPrinter.js
+++ www/NGXPrinter.js
@@ -10,7 +10,7 @@ module.exports = {
             }]
         ); 
     },
-    showDeviceList: function(){
+    showDeviceList: function(successCallback,errorCallback){
         cordova.exec(
             successCallback,
             errorCallback,
